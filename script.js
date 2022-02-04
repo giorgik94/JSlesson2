806ec7486b187b8946c84eba75427e74e471d902
Giorgi Karosanidze <giorgik94@outlook.com>
2022-02-04 16:10:56 +0400

    Final Cut

---
+++ script.js
@@ -0,0 +1,81 @@
+//დავალება 1
+
+// function numbers(...args) {
+//     let numbersSum = 0;
+//     for (let item of args) {
+//         if (item > 0) {
+//             numbersSum = item + numbersSum
+//         }
+//     }
+//     return numbersSum
+// }
+
+// let result = numbers(2, -9, 5, 11, -30, 100, -8, -11, -4, 5, -6, 8);
+// console.log(result)
+
+// let number = (...args) => {
+//     let numbersSum = 0;
+//     for (let item of args) {
+//         if (item > 0) {
+//             numbersSum = numbersSum + item
+//         }
+//     }
+//     return numbersSum
+// }
+
+// let result = number(2, -9, 5, 11, -30, 100, -8, -11, -4, 5, -6, 8);
+// console.log(result);
+
+//-----------------------------------------------------------------------------
+
+//დავალება 2
+
+// let numbers = (...args) => {
+//     numbersSum = 0;
+//     for (let item of args) {
+//         numbersSum = item + numbersSum;
+//     }
+//     return numbersSum
+// }
+
+// let result = numbers(10, 50, 6, 7, 8, 11, 6, 3, 9);
+// console.log(result);
+
+//--------------------------------------------------------------------------------
+
+//დავალება 3
+//ფუნქციამ უნდა დააბრუნოს სახელი და გვარი ადგილის გამოტოვებით (მაგ: 'giorgi saakadze')
+// თუ isloggedin არის true და false თუ isloggedin ფროფერთი არის false;
+
+// let user = {
+//     firstname: 'Giorgi',
+//     lastname: 'Karosanidze',
+//     age: 27,
+//     isloggedin: true
+// }
+
+// function names() {
+//     if (user.isloggedin === true) {
+//         return `${user.firstname} ${user.lastname}`;
+//     } else {
+//         return "There is an Error"
+//     }
+//     return
+// }
+
+// let result = names();
+// console.log(result);
+
+//----------------------------------------------------------------------------------
+
+//დავალება 4
+
+function number(...args) {
+
+    //?????????????????????????????????
+}
+
+let result = number(2, 4, 6, 8, 10);
+console.log(result);
+
+//??????????????????????????????????
